0ff35c36826596fd64efa68a0e372d60e2b2085c
Jun <amares9100@gamil.com>
2023-01-13 14:48:07 +0900

    01/13 14:47 과제3 주석추가

--- frontend/public/과제/JS과제3.js
+++ frontend/public/과제/JS과제3.js
@@ -12,25 +12,25 @@
 		
 
 		
-let contentArray = []
+let contentArray = []  // 누적기록
 
 
 function addContent() {
 let table1 = '<tr> <th>번호</th> <th>방문록</th>';
-let centent = document.querySelector('.centent').value
+let centent = document.querySelector('.centent').value // 인풋으로 입력받은 값 저장
 
-contentArray.push(centent)
+contentArray.push(centent) // 저장된 값 contentArray에 기록
 console.log(contentArray)
 
-let num = contentArray.length
+let num = contentArray.length // contentArray에 저장된 값의 길이를 num에 입력
 console.log(num)
 
-for(let 번호 = 1 ; 번호<=(num) ; 번호++){
+for(let 번호 = 1 ; 번호<=(num) ; 번호++){ // 번호=1이고 번호가 num보다 작거나 같을때 번호는 1씩 증가
 
-	
-	table1 += '<tr> <th>'+번호+' </th> <th>'+contentArray[번호]+'</th></tr>';
+	// 테이블1에 테이블과 번호, contentArray[번호-1] 내용을 (인덱스는 0부터 시작하기때문에 -1 추가) 입력
+	table1 += '<tr> <th>'+번호+' </th> <th>'+contentArray[번호-1]+'</th></tr>';
 	
 }	
-
+	// 클래스 방문록에 table1을 출력
 document.querySelector('.방문록').innerHTML = table1
 }
