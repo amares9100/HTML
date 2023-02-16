fbc1289a4b5b353a02bb8e334011e967f28c9802
amares9100 <120459763+amares9100@users.noreply.github.com>
2023-02-16 15:07:21 +0900

    Add files via upload

---
+++ backend/src/Car/model/소형.java
@@ -0,0 +1,34 @@
+package model;
+
+public class 소형 extends Car{
+	
+	
+	
+	public 소형(String string, int km, int price , int 지역) {
+		super(string , km , price , 지역);
+	}
+
+	@Override
+	public int price() {
+		System.out.println("소형차가 목적지에 도착했습니다.");
+		System.out.println("소형차 요금은"+(price*1)+"입니다.");
+		return 0;
+	}
+
+	@Override
+	public int run() {
+		System.out.println("소형차가 달립니다.");
+		지역 = 지역-km;
+		if(0 < 지역) {
+			
+			System.out.println("목적지까지"+지역+"만큼 남았습니다.");
+			return 1;
+		}
+		else { return 0;
+		}
+	}
+
+	
+	
+	
+}
